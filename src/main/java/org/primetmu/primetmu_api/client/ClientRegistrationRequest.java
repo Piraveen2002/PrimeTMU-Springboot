42cc0adc722ae1674d8d99528199f56320efd76d
Piraveen2002 <146127350+Piraveen2002@users.noreply.github.com>
2025-05-04 15:32:33 -0400

    Delete src/main directory

--- src/main/java/org/primetmu/primetmu_api/client/ClientRegistrationRequest.java
+++
@@ -1,8 +0,0 @@
-package org.primetmu.primetmu_api.client;
-
-public record ClientRegistrationRequest(
-        String firstName,
-        String lastName,
-        String username,
-        String password) {
-}
--- src/main/java/org/primetmu/primetmu_api/client/ClientUpdateRequest.java
+++
@@ -1,7 +0,0 @@
-package org.primetmu.primetmu_api.client;
-
-public record ClientUpdateRequest(
-                String firstName,
-                String lastName,
-                String username) {
-}
